6b762a7bfbfc7163fb53828d5066139d6e2a773e
Kwon Da Woon <momnpa333@gmail.com>
2024-01-22 15:32:53 +0900

    Feat:axios 설정 변경

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // dotenv.config();
 
 export const axiosInstance = axios.create({
-    //baseURL: import.meta.env.PROD ? "" : "http://3.131.159.236:8080",
-    baseURL: "http://3.131.159.236:8080",
+    baseURL: process.env.BACKEND_SERVER || "http://back:8080",
+    //baseURL: "http://3.131.159.236",
     //    baseURL: import.meta.env.PROD ? "" : "http://localhost:8080",
 });
 
