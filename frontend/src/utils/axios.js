12024d895d4cd0ee5ca2af81cda4d853def7ac76
Kwon Da Woon <momnpa333@gmail.com>
2024-01-22 16:38:11 +0900

    Feat: log추가

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -8,9 +8,6 @@ export const axiosInstance = axios.create({
     //baseURL: import.meta.env.PROD ? "" : "http://localhost:8080",
 });
 
-// 추가 코드
-console.log("BACKEND_SERVER:", process.env.BACKEND_SERVER);
-
 export const testInstance = axios.create({
     baseURL: import.meta.env.PROD ? "" : "http://localhost:8080",
 });
